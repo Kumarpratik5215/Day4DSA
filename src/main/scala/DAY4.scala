233965ecf461a70650e7c959bbb1ed642f89c116
Kumarpratik5215 <111360638+Kumarpratik5215@users.noreply.github.com>
2024-04-26 16:09:38 +0530

    first commit

---
+++ src/main/scala/DAY4.scala
@@ -0,0 +1,36 @@
+object DAY4 {
+
+  def genParentheses(n: Int): List[String] = {
+    def genHelper(open: Int, close: Int, current: String): List[String] = {
+      if (open == n && close == n) {
+        List(current)
+      } else {
+        var res = List[String]()
+        if (open < n)
+          res = res ++ genHelper(open + 1, close, current + "(")
+        if (close < open) {
+          res = res ++ genHelper(open, close + 1, current + ")")
+        }
+        res
+      }
+    }
+    genHelper(0, 0, "")
+  }
+
+  def main(args: Array[String]): Unit = {
+    val n = 3
+    val comb = genParentheses(n)
+    val validcomb = comb.filter(DAY4.isValid)
+    validcomb.foreach(println)
+  }
+
+  def isValid(str: String): Boolean = {
+    def isValidHelper(str: String, balanced: Int): Boolean = {
+      if (balanced < 0) false
+      else if (str.isEmpty) balanced == 0
+      else if (str.head == '(') isValidHelper(str.tail, balanced + 1)
+      else isValidHelper(str.tail, balanced - 1)
+    }
+    isValidHelper(str, 0)
+  }
+}
